bf35656f3757a4fa8e752fa5d5fe19e8d11171fa
rodrigomint <rodrigomint@rodrigomint-VirtualBox.(none)>
2012-10-21 17:47:14 +0100

    piedra papel tijeras

---
+++ ppt.rb
@@ -0,0 +1,25 @@
+defeat = { :rock => :scissors,
+           :paper => :rock, 
+					 :scissors => :paper}
+throws = defeat.keys
+
+if ARGV.size == 0 
+  puts "Choose one of #{throws.join(', ')}: "
+  player_throw = gets.chomp.to_sym
+else 
+  player_throw = (ARGV.shift || '').to_sym
+end
+
+raise ScriptError, "You must execute: ''#{$0}'' followed by one of the following '#{throws.join(', ')}'" unless throws.include? player_throw
+
+computer_throw = throws.sample
+
+if player_throw == computer_throw
+  answer = "There is a tie"
+elsif player_throw == defeat[computer_throw]
+  answer = "Computer wins; #{computer_throw} defeats #{player_throw}"
+else
+  answer = "Well done. #{player_throw} beats #{computer_throw}"
+end
+puts answer
+
